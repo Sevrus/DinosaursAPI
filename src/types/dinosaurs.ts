bbaf7ed7e144efd645ef10ac22b4891382ed4278
Sevrus <book-n-troll@nordnet.fr>
2026-03-04 15:20:38 +0100

    Define `Dinosaur` and `DinoText` types for consistent type-checking across dinosaur data structures.

---
+++ src/types/dinosaurs.ts
@@ -0,0 +1,20 @@
+export type DinoText = {en: string; fr: string};
+
+export type Dinosaur = {
+    name: string;
+    period: DinoText;
+    diet: DinoText;
+    classification: {
+        kingdom: string;
+        phylum: string;
+        class: string;
+        order: string;
+        suborder: string;
+        family: string;
+        genus: string;
+        species: string;
+    };
+    length_m: number;
+    discovered_in: string;
+    description: DinoText;
+}
